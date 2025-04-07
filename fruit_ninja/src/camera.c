d528c006baab6b8c9351ee685a04063ac48b3100
Zarándi Ákos <zarandi.akos@student.uni-miskolc.hu>
2025-04-07 18:16:07 +0200

    a kód formázása

--- fruit_ninja/src/camera.c
+++ fruit_ninja/src/camera.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <math.h>
 
-void init_camera(Camera* camera)
+void init_camera(Camera *camera)
 {
     camera->position.x = 0.0;
     camera->position.y = 0.0;
@@ -16,7 +16,7 @@ void init_camera(Camera* camera)
     camera->speed.z = 0.0;
 }
 
-void update_camera(Camera* camera, double time)
+void update_camera(Camera *camera, double time)
 {
     double angle;
     double side_angle;
@@ -27,22 +27,26 @@ void update_camera(Camera* camera, double time)
     camera->position.y += sin(angle) * camera->speed.y * time;
     camera->position.x += cos(side_angle) * camera->speed.x * time;
     camera->position.y += sin(side_angle) * camera->speed.x * time;
-	
-	if(camera->position.x>5.0){
-		camera->position.x=5.0;
-	}
-	if(camera->position.x<-5.0){
-		camera->position.x=-5.0;
-	}
-	if(camera->position.y>4.0){
-		camera->position.y=4.0;
-	}
-	if(camera->position.y<-35.0){
-		camera->position.y=-35.0;
-	}
+
+    if (camera->position.x > 5.0)
+    {
+        camera->position.x = 5.0;
+    }
+    if (camera->position.x < -5.0)
+    {
+        camera->position.x = -5.0;
+    }
+    if (camera->position.y > 4.0)
+    {
+        camera->position.y = 4.0;
+    }
+    if (camera->position.y < -35.0)
+    {
+        camera->position.y = -35.0;
+    }
 }
 
-void set_view(const Camera* camera)
+void set_view(const Camera *camera)
 {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -52,38 +56,41 @@ void set_view(const Camera* camera)
     glTranslatef(-camera->position.x, -camera->position.y, -camera->position.z);
 }
 
-void rotate_camera(Camera* camera, double horizontal, double vertical)
+void rotate_camera(Camera *camera, double horizontal, double vertical)
 {
-    camera->rotation.z += horizontal/10;
-    camera->rotation.x += vertical/10;
+    camera->rotation.z += horizontal / 10;
+    camera->rotation.x += vertical / 10;
 
-    if (camera->rotation.z < 0) {
+    if (camera->rotation.z < 0)
+    {
         camera->rotation.z += 360.0;
     }
 
-    if (camera->rotation.z > 360.0) {
+    if (camera->rotation.z > 360.0)
+    {
         camera->rotation.z -= 360.0;
     }
-	
-	if (camera->rotation.x < -90.0) {
+
+    if (camera->rotation.x < -90.0)
+    {
         camera->rotation.x = -90.0;
     }
 
-    if (camera->rotation.x > 90.0) {
-        camera->rotation.x =90.0;
+    if (camera->rotation.x > 90.0)
+    {
+        camera->rotation.x = 90.0;
     }
 }
 
-void set_camera_speed(Camera* camera, double speed)
+void set_camera_speed(Camera *camera, double speed)
 {
     camera->speed.y = speed;
 }
 
-void set_camera_side_speed(Camera* camera, double speed)
+void set_camera_side_speed(Camera *camera, double speed)
 {
     camera->speed.x = speed;
 }
-
 
 void f1_panel()
 {
@@ -140,11 +147,11 @@ void middle_dot()
 vec3 camera_direction(Camera *camera)
 {
     vec3 dir = {cos(degree_to_radian(camera->rotation.x)) *
-                 cos(degree_to_radian(camera->rotation.z)), 
-                 sin(degree_to_radian(camera->rotation.z)) *
-                 cos(degree_to_radian(camera->rotation.x)),
-                 sin(degree_to_radian(camera->rotation.x))};
-                 
+                    cos(degree_to_radian(camera->rotation.z)),
+                sin(degree_to_radian(camera->rotation.z)) *
+                    cos(degree_to_radian(camera->rotation.x)),
+                sin(degree_to_radian(camera->rotation.x))};
+
     float w = sqrt(dir.x * dir.x + dir.y * dir.y + dir.z * dir.z);
     dir.x /= w;
     dir.y /= w;
--- fruit_ninja/src/main.c
+++ fruit_ninja/src/main.c
@@ -4,15 +4,16 @@
 /**
  * Main function
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     App app;
-	argc=argc;
-	argv=argv;
-	app.width=1920;
-	app.heigth=1080;
+    argc = argc;
+    argv = argv;
+    app.width = 1920;
+    app.heigth = 1080;
     init_app(&app, app.width, app.heigth);
-    while (app.is_running) {
+    while (app.is_running)
+    {
         handle_app_events(&app);
         update_app(&app);
         render_app(&app);
